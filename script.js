475f1f4184bfe74bac40ec9efa44f07e24eac397
Sham-121 <samita.khanduri.qu@gmail.com>
2026-08-14 11:11:12 +0530

    basic ui done

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//our choices
+const num1 = document.querySelector(".num1");
+const operator = document.querySelector(".operator");
+const num2 = document.querySelector(".num2");
+const result = document.querySelector(".result");
+
+
+let buttons = document.querySelectorAll('.num');
+for (let i=0; i< buttons.length; i++){
+  let button = buttons[i];
+  button.addEventListener('click' , () => {
+    document.getElementById("display").innerHTML += button.innerHTML
+  })
+}
+
+
+
 const add = function(a,b){
     return a+b;
 }
@@ -13,3 +30,5 @@ const multiply = function(a,b){
 const divide = function(a,b){
     return a/b;
 }
+
+
