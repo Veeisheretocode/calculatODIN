b43a01f47f6e61d7310d2dea5f671f9d0b3cf2bd
Sham-121 <samita.khanduri.qu@gmail.com>
2026-08-14 11:58:24 +0530

    only add operator to display if display not empty

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
+//check if display is empty
+function isDisplayEmpty() {
+    const display = document.querySelector(".display");
+    return display.textContent == '';
+}
+
 //function to get stuff we click to show up on display
 let nums = document.querySelectorAll('.num');
-for (let i=0; i< nums.length; i++){
-  let num = nums[i];
-  num.addEventListener('click' , () => {
-    document.getElementById("display").textContent += num.textContent;
-  });
+for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    num.addEventListener('click', () => {
+        document.getElementById("display").textContent += num.textContent;
+    });
 };
 
 let ops = document.querySelectorAll('.op');
-for (let i=0; i< ops.length; i++){
-  let op = ops[i];
-  op.addEventListener('click' , () => {
-    document.getElementById("display").textContent += op.textContent;
-  });
+for (let i = 0; i < ops.length; i++) {
+    let op = ops[i];
+    op.addEventListener('click', () => {
+        if (!isDisplayEmpty()) {
+            document.getElementById("display").textContent += op.textContent;
+        }
+    });
 };
 
 //function to clear display
-function clearDisplay(){
-    document.getElementById("display").textContent= "";
+function clearDisplay() {
+    document.getElementById("display").textContent = "";
 }
 const clear = document.querySelector(".clear");
 clear.addEventListener("click", clearDisplay);
 
 //function to use backsapce
-function backSpace(){
+function backSpace() {
     const displayElement = document.getElementById("display");
     displayElement.textContent = displayElement.textContent.slice(0, -1);
 }
