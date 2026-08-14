8dd54a36be7b13ac3e88bd5ea1d20880059e1e81
Sham-121 <samita.khanduri.qu@gmail.com>
2026-08-14 13:42:41 +0530

    calculate function, that executes when click on equal, handle decimal numbers, parse int, detects operator symbol and map it to operations, where we have mapped the symbols to functions

--- script.js
+++ script.js
@@ -4,71 +4,86 @@ function isDisplayEmpty() {
     return display.textContent == '';
 }
 
-//function to get stuff we click to show up on display
+//get stuff we click to show up on display
 let nums = document.querySelectorAll('.num');
-for (let i = 0; i < nums.length; i++) {
-    let num = nums[i];
+nums.forEach((num) => {
     num.addEventListener('click', () => {
-        document.getElementById("display").textContent += num.textContent;
+        document.getElementById("display").textContent += num.textContent; //add what you click into display
     });
-};
+})
 
 let ops = document.querySelectorAll('.op');
-for (let i = 0; i < ops.length; i++) {
-    let op = ops[i];
+ops.forEach((op) => {
     op.addEventListener('click', () => {
-        if (!isDisplayEmpty()) {
+        if (!isDisplayEmpty()) { //check if display empty EVERYTIME we click an operator
             document.getElementById("display").textContent += op.textContent;
         }
     });
-};
+})
 
-//function to clear display
+//clear display
 function clearDisplay() {
-    document.getElementById("display").textContent = "";
+    document.getElementById("display").textContent = ""; //clears text
 }
 const clear = document.querySelector(".clear");
 clear.addEventListener("click", clearDisplay);
 
-//function to use backsapce
+//use backsapce
 function backSpace() {
     const displayElement = document.getElementById("display");
-    displayElement.textContent = displayElement.textContent.slice(0, -1);
+    displayElement.textContent = displayElement.textContent.slice(0, -1); //slice text 1 at a time
 }
 const backspace = document.querySelector(".backspace");
 backspace.addEventListener("click", backSpace);
 
+//OPERTIONS-
+
+const add = (items) => items.reduce((acc, curr) => acc + curr, 0);
+
+const subtract = (items) => items.reduce((diff, num, index) => index === 0 ? num : diff - num, 0);
+
+const multiply = (items) => items.reduce((acc, curr) => acc * curr, 1);
+
+const divide = (items) => items.reduce((quoti, num, index) => {
+    if (index === 0) return num;
+    if (num === 0) return NaN;
+    return quoti / num;
+});
+
+//map buttons to functions
+
+const operators = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide
+};
+
+//calculate results
+
+function calculate() {
+    const userInput = document.getElementById("display").textContent;
+    const userInputTokens = userInput.match(/\d+\.\d+|\d+|\+|-|\*|\//g); //regex parser
+
+    if (!userInputTokens) return;
+
+    const finalResult = userInputTokens.reduce((acc, curr, ind) => {
+        if(ind%2 == 0) return acc;
+
+        const operator = curr;
+
+         const nextNumber = parseFloat(userInputTokens[ind + 1]);
 
 
+        const mathOperation = operators[operator];
+        return mathOperation([acc, nextNumber]);
+    }, parseFloat(userInputTokens[0]));
 
+    return document.getElementById("display").textContent = finalResult;
+}
 
+const equal = document.querySelector(".equals");
+equal.addEventListener("click", calculate);
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const add = function(a,b){
-//     return a+b;
-// }
-
-// const subtract = function(a,b){
-//     return a-b;
-// }
-
-// const multiply = function(a,b){
-//     return a*b;
-// }
-
-// const divide = function(a,b){
-//     return a/b;
-// }
 
 
