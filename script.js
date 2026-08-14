55fffb035e122866c77b3fa90ffaa36d291583aa
Sham-121 <samita.khanduri.qu@gmail.com>
2026-08-14 11:26:27 +0530

    display nums and operators when clicking on them

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
 //our choices
-const num1 = document.querySelector(".num1");
-const operator = document.querySelector(".operator");
-const num2 = document.querySelector(".num2");
-const result = document.querySelector(".result");
+// const num1 = document.querySelector(".num1");
+// const operator = document.querySelector(".operator");
+// const num2 = document.querySelector(".num2");
+// const result = document.querySelector(".result");
 
 
-let buttons = document.querySelectorAll('.num');
-for (let i=0; i< buttons.length; i++){
-  let button = buttons[i];
-  button.addEventListener('click' , () => {
-    document.getElementById("display").innerHTML += button.innerHTML
-  })
-}
+//function to get stuff we click to show up on display
+let nums = document.querySelectorAll('.num');
+for (let i=0; i< nums.length; i++){
+  let num = nums[i];
+  num.addEventListener('click' , () => {
+    document.getElementById("display").textContent += num.textContent;
+  });
+};
+
+let ops = document.querySelectorAll('.op');
+for (let i=0; i< ops.length; i++){
+  let op = ops[i];
+  op.addEventListener('click' , () => {
+    document.getElementById("display").textContent += op.textContent;
+  });
+};
 
 
+// const add = function(a,b){
+//     return a+b;
+// }
 
-const add = function(a,b){
-    return a+b;
-}
+// const subtract = function(a,b){
+//     return a-b;
+// }
 
-const subtract = function(a,b){
-    return a-b;
-}
+// const multiply = function(a,b){
+//     return a*b;
+// }
 
-const multiply = function(a,b){
-    return a*b;
-}
-
-const divide = function(a,b){
-    return a/b;
-}
+// const divide = function(a,b){
+//     return a/b;
+// }
 
 
